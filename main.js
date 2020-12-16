673f7882307a22a5a51b804b5b0be4b52ddfd57b
No_otra_vez_ha <saulbector@gmail.com>
2020-12-15 19:47:02 -0600

    Añade index y js de Tofu-mef-y-satie

---
+++ main.js
@@ -0,0 +1,13 @@
+$(function() {
+  $('#video').css({
+    width: $(window).innerWidth() + 'px',
+    height: $(window).innerHeight() + 'px'
+  });
+
+  $(window).resize(function() {
+    $('#video').css({
+      width: $(window).innerWidth() + 'px',
+      height: $(window).innerHeight() + 'px'
+    });
+  });
+});
